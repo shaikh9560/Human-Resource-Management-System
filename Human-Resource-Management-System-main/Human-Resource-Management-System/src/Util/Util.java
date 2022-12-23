0f6e0482eedfd8e25deb5c154c602b7ea1f119e8
Musavvir Shaikh <104588080+shaikh9560@users.noreply.github.com>
2022-12-23 19:08:05 +0530

    Update Util.java

--- Human-Resource-Management-System-main/Human-Resource-Management-System/src/Util/Util.java
+++ Human-Resource-Management-System-main/Human-Resource-Management-System/src/Util/Util.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+/*--------------------------------------------     Connection making with SQL    -----------------------------------------------*/
+
 public class Util {
 
 	public static Connection getConnection() {
