95f7a2bc310420c889ed83e7b2003f5f43130736
Musavvir Shaikh <104588080+shaikh9560@users.noreply.github.com>
2022-12-23 19:06:16 +0530

    Update view_All_Employee_Admin.java

--- Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/view_All_Employee_Admin.java
+++ Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/view_All_Employee_Admin.java
@@ -7,6 +7,9 @@ import DAO.EmployeeDaoImp;
 import Exceptions.Employee_Exception;
 import JavaBean.Employee;
 
+
+/*--------------------------------------------    view List of employee   -----------------------------------------------*/
+
 public class view_All_Employee_Admin {
 
 	public static void main(String[] args) {
