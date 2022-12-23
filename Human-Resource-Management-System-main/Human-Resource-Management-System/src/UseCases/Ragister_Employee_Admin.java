d5e76a77b8b99cdf2a873727f478a97c82308696
Musavvir Shaikh <104588080+shaikh9560@users.noreply.github.com>
2022-12-23 19:04:27 +0530

    Update Ragister_Employee_Admin.java

--- Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/Ragister_Employee_Admin.java
+++ Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/Ragister_Employee_Admin.java
@@ -5,6 +5,9 @@ import DAO.EmployeeDaoImp;
 import Exceptions.Employee_Exception;
 import JavaBean.Employee;
 
+
+/*--------------------------------------------     Register Employee By Admin   -----------------------------------------------*/
+
 public class Ragister_Employee_Admin {
 
 	public static void main(String[] args) {
