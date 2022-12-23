37a21d5a34dd7e839145a202c6d0f9c2ae7ab117
Musavvir Shaikh <104588080+shaikh9560@users.noreply.github.com>
2022-12-23 19:02:21 +0530

    Update Leave_Accept_Admin.java

--- Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/Leave_Accept_Admin.java
+++ Human-Resource-Management-System-main/Human-Resource-Management-System/src/UseCases/Leave_Accept_Admin.java
@@ -3,6 +3,8 @@ package UseCases;
 import DAO.EmployeeDao;
 import DAO.EmployeeDaoImp;
 
+/*--------------------------------------------     Accept Leave By Admin    -----------------------------------------------*/
+
 public class Leave_Accept_Admin {
 	public static void main(String[] args) {
 		
