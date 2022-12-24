a160a8057b6b030397a6968009ab1f68361deb6c
Musavvir Shaikh <104588080+shaikh9560@users.noreply.github.com>
2022-12-24 14:22:06 +0530

    Exception Handling

--- Human-Resource-Management-System-main/Human-Resource-Management-System/src/Exceptions/Department_Exception.java
+++ Human-Resource-Management-System-main/Human-Resource-Management-System/src/Exceptions/Department_Exception.java
@@ -2,7 +2,7 @@ package Exceptions;
 
 public class Department_Exception extends Exception {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID  = 1L;
 	
 	public Department_Exception() {
 		
